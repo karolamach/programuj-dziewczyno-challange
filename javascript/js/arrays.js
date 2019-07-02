d7e5781088b53a8776b82a657fb5657708dcd90d
Karolina Machniewska <karolina.machniewska@gmail.com>
2019-07-02 18:50:26 +0200

    index.html file changed

---
+++ javascript/js/arrays.js
@@ -0,0 +1,115 @@
+//arrays
+let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
+
+console.log(newYearsResolutions);
+
+//Create an Array
+
+const hobbies = ["swimming","windsurfing","reading"];
+
+console.log(hobbies);
+
+//Accessing Elements
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+const listItem = famousSayings[0];
+console.log(listItem)
+console.log(famousSayings[2]);
+
+console.log(famousSayings[3]);
+
+//Update Elements
+let groceryList = ['bread', 'tomatoes', 'milk'];
+
+groceryList[1] = "avocados";
+
+//Arrays with let and const
+
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+condiments[0] = 'Mayo';
+
+console.log(condiments);
+
+condiments = ['Mayo'];
+
+utensils[3] = 'Spoon';
+
+console.log(utensils);
+
+// utensils = ['Spork'];
+
+
+//The .length property
+const objectives = ['Learn a new languages', 'Read 52 books', 'Run a marathon'];
+console.log(objectives.length);
+
+//The .push() Method
+const chores = ['wash dishes', 'do laundry', 'take out trash'];
+
+chores.push("do your bed","walk the dog");
+
+console.log(chores)
+
+//The .pop() Method
+const chores = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+
+chores.pop(); //usuwa ostatni element z listy 
+
+console.log(chores)
+
+//More Array Methods
+
+const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+groceryList.shift(); //usuwa z listy pierwszy element
+console.log(groceryList)
+
+groceryList.unshift("popcorn") //dodaje element na poczatek listy
+
+console.log(groceryList);
+
+console.log(groceryList.slice(1, 4)); //dzieli liste argumenty to numer indexu początkowego skladnika listy i ostatniego +1
+
+console.log(groceryList);
+
+const pastaIndex = groceryList.indexOf('pasta'); // pokazuje numer indexu wskazanego elementu
+
+console.log(pastaIndex);
+
+
+//Arrays and Functions
+
+const flowers = ['peony', 'daffodil', 'marigold'];
+
+function addFlower(arr) {
+  arr.push('lily');
+}
+
+addFlower(flowers);
+
+console.log(flowers); // Output: ['peony', 'daffodil', 'marigold', 'lily']
+
+const concept = ['arrays', 'can', 'be', 'mutated'];
+
+function changeArr(arr){
+  arr[3] = 'MUTATED';
+}
+
+changeArr(concept);
+console.log(concept);
+
+function removeElement(newArr){
+  newArr.pop()
+}
+ removeElement(concept)
+console.log(concept);
+
+
+// Nested Arrays (zagniezdzone)
+
+const numberClusters = [[1,2],[3,4],[5,6]];
+// console.log(numberClusters)
+const target = numberClusters[2][1];
+console.log(target);
